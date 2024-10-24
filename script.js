55ba4194f52c362872b41a0d4cd9cf3ca966fb24
Juliette Suc <juliette.suc@outlook.fr>
2024-10-24 16:45:26 +0200

    fix : all search icon link to feed-search page with search modal already open

--- script.js
+++ script.js
@@ -26,13 +26,12 @@ async function getAllBooks (query) {
     const API_URL_SEARCH = 'https://www.googleapis.com/books/v1/volumes?q=';
 
     try {
-        const response = await fetch(API_URL_SEARCH+query+'&maxResults=40');
+        const response = await fetch(API_URL_SEARCH+query+'&maxResults=40&orderBy=relevance');
         if (!response.ok) {
             throw new Error (`Response status: ${response.status}`);
         }
         const data = await response.json();
         const bookFound = data.items;
-        console.log(bookFound);
         return bookFound;
         
     } catch (error) {
@@ -46,9 +45,13 @@ async function getAllBooks (query) {
             
     form.addEventListener('submit', (event) => {
         event.preventDefault();
+         if(window.location.href !== "http://localhost:5500/feed.html"){
+            window.location.href = "http://localhost:5500/feed.html"  
+            }
         if(nameInput){
            const searchName = nameInput.value;
            getAllBooks(searchName).then(bookFound => {
+           
             const resultDiv = document.getElementById('searchResult');
             resultDiv.innerHTML= '<h2>Results</h2>';
             bookFound.forEach(result => {
@@ -60,6 +63,7 @@ async function getAllBooks (query) {
                 console.log(result.volumeInfo.imageLinks)
                 
             });
+            nameInput.value = '';
             document.getElementById("search").style.display = "none"; 
         });;
         }
